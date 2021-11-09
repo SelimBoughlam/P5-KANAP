59f3910eb2fbc68626d66c67fd2d649f322bdaa0
selim boughlam <selimboughlam@gmail.com>
2021-11-09 13:41:04 +0100

    ajout de commentaires sur la page product.js

--- front/JS/product.js
+++ front/JS/product.js
@@ -2,6 +2,15 @@
 
 getProducts();
 let product;
+
+/**
+ *  Fonction principale
+ * assignation de l'id récupéré dans une constante
+ * requête get à l'api en incluant l'id récupréré
+ * Appel de la fonction d'affichage des produits
+ * Appel de la fonction qui permet la sélection des couleurs
+ * Appel de la fonction qui ajoute les articles sélectionnés dans le panier
+ */
 async function getProducts() {
   const productId = getProductId();
   await fetch(`http://localhost:3000/api/products/${productId}`).then((res) =>
@@ -12,10 +21,20 @@ async function getProducts() {
   addToCart();
 }
 
+/**
+ * Récupère l'id du produit dans l'URL
+ */
 function getProductId() {
   return new URL(document.location).searchParams.get("id");
 }
 
+/**
+ *      Fonction affichant le produit et ses caractéristiques à l'écran
+ * création et affichage de l'image
+ * création et affichage du titre
+ * création et affichage du prix
+ * création et affichage de la description du produit
+ */
 function displayProducts() {
   let imgDiv = document.querySelector(".item__img");
 
@@ -34,6 +53,11 @@ function displayProducts() {
   description.innerHTML = product.description;
 }
 
+/**
+ *    Fonction permettant la sélection des couleurs
+ * récupération des couleurs disponibles dans l'api pour chaque produit
+ * création d'options correspondant aux couleurs pour le tag select
+ */
 function colorSelection() {
   let colors = product.colors;
   colors.forEach((color) => {
@@ -45,6 +69,15 @@ function colorSelection() {
   });
 }
 
+/**
+ *     Fonction permettant l'ajout du produit sélectionné dans le panier
+ * création objet qui contient l'article choisi
+ * création d'un tableau vide qui représente le panier
+ * SI le localStorage est vide: push un article dans le tableau panier et afficher ce tableau dans le localStorage
+ * déclaration du LocalStorage en variable storage
+ * pour chaque article présent dans le localStorage(storage):si l'ID et la couleur sont les mêmes=>on ajuste la quantité,sinon on ajoute un nouvel article
+ *
+ */
 function addToCart() {
   let addToCart = document.getElementById("addToCart");
   let quantity = document.getElementById("quantity");
@@ -80,16 +113,3 @@ function addToCart() {
     }
   });
 }
-
-// AMELIORATION:
-// message d'erreur quand pas de couleurs sélectionnées
-
-// storage.forEach((data) => {
-//   if (article.id == data.id && article.color == data.color) {
-//     article.quantity = article.quantity + data.quantity;
-//   } else {
-//     cart.push(data);
-//   }
-// });
-// cart.push(article);
-// localStorage.setItem("articlesInCart", JSON.stringify(cart));
